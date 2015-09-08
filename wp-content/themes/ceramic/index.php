c89e2834c48363364249d3aa6d2ff0d0f874c234
SonVQ <sonvq@qsoft.com.vn>
2015-09-08 22:52:16 +0700

    commit

--- wp-content/themes/ceramic/index.php
+++ wp-content/themes/ceramic/index.php
@@ -6,6 +6,22 @@ if (have_posts()) :
         ?>
         <article class="post">
             <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            <p class="post-info">
+                <?php the_time('F j, Y g:1 a'); ?> | by <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author(); ?></a> | Posted in 
+                    <?php
+                        $categories = get_the_category();
+                        $separator = ", ";
+                        $output = '';
+                        
+                        if($categories) {
+                            foreach ($categories as $category) {
+                                $output .= '<a href="' . get_category_link($category->term_id). '">' . $category->cat_name . '</a>' . $separator;
+                            }
+                            
+                            echo trim($output, $separator);
+                        }
+                    ?>
+            </p>
             <?php the_content(); ?>
         </article>
         <?php
